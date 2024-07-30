1d926e95a72032fceb857ef901e93e82aadff87b
anurag357 <pathakanurag753@gmail.com>
2024-07-30 17:49:55 +0530

    add database confirmetion

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,14 @@
+require('dotenv').config()
+require("./config/database").connect()
+const {PORT} = process.env
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const admin = require('firebase-admin');
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-
-const serviceAccount = require('./path/to/serviceAccountKey.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/flight-status', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
 // Define a simple Flight schema
 const flightSchema = new mongoose.Schema({
@@ -47,7 +37,6 @@ app.post('/api/subscribe/:id', (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT , () => {
+  console.log(`Server is running port on http://localhost/${PORT}`)
+})
